486d77a7a77e6662b6ba16bbe7111a7228e684ce
Olivier PEUGNET <opeugnet@hotmail.com>
2025-01-31 10:55:55 +0000

    Fix;coma issue in stg_sales_database__orders. Add: int_sales_database__orders

---
+++ models/intermediate/sales_datebase/int_sales_database__orders.sql
@@ -0,0 +1,21 @@
+WITH order_items_grouped_by_orders AS (
+  SELECT order_id,
+  SUM(total_order_item_amount) AS total_order_amount,
+  SUM(total_order_item_amount_with_discount) AS total_order_amount_after_discount,
+  SUM(item_quantity) AS total_items,
+  COUNT(DISTINCT product_id) AS total_distinct_items
+FROM {{ref ('stg_sales_database__order_items')}}
+GROUP BY order_id
+)
+
+SELECT o.order_id,
+  o.customer_id,
+  o.store_id,
+  o.staff_id,
+  o.order_created_at,
+  COALESCE(oi.total_order_amount,0) AS total_order_amount,
+  COALESCE(oi.total_order_amount_after_discount,0) AS total_order_amount_after_discount,
+  COALESCE(oi.total_items,0) AS total_items,
+  COALESCE(oi.total_distinct_items,0) AS total_distinct_items
+FROM {{ref ('stg_sales_database__orders')}} AS o
+LEFT JOIN order_items_grouped_by_orders AS oi ON o.order_id = oi.order_id
--- models/staging/sales_database/stg_sales_database__orders.sql
+++ models/staging/sales_database/stg_sales_database__orders.sql
@@ -2,7 +2,7 @@ SELECT
   order_id,
   customer_id,
   store_id,
-  staff_id
+  staff_id,
   order_status,
   order_date AS order_created_at,
   required_date AS order_required_at,
